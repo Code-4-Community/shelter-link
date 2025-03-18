1e93b2581abb36b004310a30f966637448241c57
neetidesai <97416423+neetidesai@users.noreply.github.com>
2025-03-18 11:49:53 -0400

    fixups

--- backend/src/user/user.model.ts
+++ backend/src/user/user.model.ts
@@ -5,6 +5,7 @@
  * @property first_name - The first name of the user.
  * @property last_name - The last name of the user.
  * @property email - The email of the user.
+ * @property created_at - The date and time the user was created.
  *
  */
 export type UserModel = {
@@ -12,6 +13,13 @@ export type UserModel = {
   first_name: string;
   last_name: string;
   email: string;
+  created_at: string;
 };
 
-export type UserInputModel = {};
+export type UserInputModel = {
+  userId: { S: string };
+  first_name: { S: string };
+  last_name: { S: string };
+  email: { S: string };
+  created_at: { S: string };
+};
