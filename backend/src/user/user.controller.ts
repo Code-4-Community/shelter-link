b9e2378c4fa6ae81a9c48ad1a6af82609d228448
neetidesai <97416423+neetidesai@users.noreply.github.com>
2025-03-19 16:18:05 -0400

    Add user authentication with cognito and user creation

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,7 +1,26 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  HttpStatus,
+  HttpException,
+  Post,
+  Body,
+} from '@nestjs/common';
 import { UserService } from './user.service';
+import { NewUserInput } from '../dtos/newUserDTO';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Post()
+  public async postUser(@Body() userData: NewUserInput) {
+    try {
+      return await this.userService.postUser(userData);
+    } catch (error) {
+      throw new HttpException(
+        `Unable to create user: ${error.message}`,
+        HttpStatus.BAD_REQUEST
+      );
+    }
+  }
 }
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,9 +1,84 @@
 import { Injectable } from '@nestjs/common';
 
 import { DynamoDbService } from '../dynamodb';
+import { NewUserInput } from '../dtos/newUserDTO';
+import { UserInputModel } from './user.model';
+import * as AWS from 'aws-sdk';
+
+const cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider();
 
 @Injectable()
 export class UserService {
   private readonly tableName = 'shelterlinkUsers';
   constructor(private readonly dynamoDbService: DynamoDbService) {}
+
+  public async postUser(userData) {
+    // Make sure email is unique
+    const existingUser = await this.dynamoDbService.checkIfEmailExists(
+      this.tableName,
+      userData.email
+    );
+    if (existingUser) {
+      throw new Error(
+        `A user with the email ${userData.email} already exists.`
+      );
+    }
+
+    const userModel = this.postInputToUserModel(userData);
+    const newId =
+      ((await this.dynamoDbService.getHighestId(this.tableName, 'userId')) ??
+        0) + 1;
+    userModel.userId.S = newId.toString();
+
+    // Create Cognito User
+    await this.createCognitoUser(userData.email, userData.password);
+
+    // Save to DynamoDB
+    await this.dynamoDbService.postItem(this.tableName, userModel);
+  }
+
+  /**
+   * Converts the input data to a user model suitable for DynamoDB.
+   * @param input The input data for the new user.
+   * @returns The user model.
+   */
+  private postInputToUserModel = (input: NewUserInput): UserInputModel => {
+    const newUserModel: UserInputModel = {
+      userId: { S: '0' },
+      first_name: { S: input.first_name },
+      last_name: { S: input.last_name },
+      email: { S: input.email },
+      created_at: { S: new Date().toISOString() },
+    };
+
+    return newUserModel;
+  };
+
+  private createCognitoUser = async (
+    email: string,
+    password: string
+  ): Promise<{ email: string }> => {
+    const signUpParams = {
+      ClientId: process.env.COGNITO_CLIENT_ID,
+      Username: email,
+      Password: password,
+      UserAttributes: [
+        {
+          Name: 'email',
+          Value: email,
+        },
+      ],
+    };
+    await cognitoidentityserviceprovider.signUp(signUpParams).promise();
+    const confirmParams = {
+      UserPoolId: process.env.USER_POOL_ID,
+      Username: email,
+    };
+    await cognitoidentityserviceprovider
+      .adminConfirmSignUp(confirmParams)
+      .promise();
+    return {
+      email,
+    };
+  };
 }
