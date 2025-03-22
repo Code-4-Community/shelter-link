7ca2cc8e84d6b1896ed1e617126517f08ca4d4e3
Sam Nie <147653722+SamNie2027@users.noreply.github.com>
2025-03-22 12:36:18 -0400

    initial implementation, not yet tested

--- backend/src/event/event.service.ts
+++ backend/src/event/event.service.ts
@@ -7,39 +7,70 @@ export class EventService {
   private readonly tableName = 'shelterlinkEvents';
   constructor(private readonly dynamoDbService: DynamoDbService) {}
 
+  /**
+   * Retrieve a specific event from the database.
+   * @returns a specific event
+   * @throws Error if the event cannot be retrieved
+   */
   public async getEvent(id: string) {
-    throw new Error('Method not implemented.');
+    try {
+      const data = await this.dynamoDbService.scanTable(
+        this.tableName,
+        'id = :id',
+        { ':id': { S: id } }
+      );
+      return this.eventModelToOutput(data[0]);
+    } catch (e) {
+      throw new Error('Unable to get event: ' + e);
+    }
   }
 
+  /**
+   * Retrieve all events from the database.
+   * @returns The list of events.
+   * @throws Error if the events cannot be retrieved.
+   */
   public async getEvents() {
     try {
       const data = await this.dynamoDbService.scanTable(this.tableName);
       return data.map((item) => this.eventModelToOutput(item));
     } catch (e) {
-      throw new Error('Unable to get shelters: ' + e);
+      throw new Error('Unable to get events: ' + e);
     }
   }
 
   /**
-     * Converts a shelter model from DynamoDB to a ShelterModel.
-     * @param input The input shelter model from DynamoDB.
-     * @returns The ShelterModel.
-     */
-    private eventModelToOutput = (input: EventInputModel): EventModel => {
-      const newShelterModel: EventModel = {};
-  
-      if (input.rating !== undefined) {
-        newShelterModel.rating = parseFloat(input.rating.N);
-      }
-  
-      if (input.website !== undefined) {
-        newShelterModel.website = input.website.S;
-      }
-  
-      if (input.expanded_name !== undefined) {
-        newShelterModel.expanded_name = input.expanded_name.S;
-      }
-  
-      return newShelterModel;
+   * Converts an event model from DynamoDB to a EventModel.
+   * @param input The input event model from DynamoDB.
+   * @returns The EventModel.
+   */
+  private eventModelToOutput = (input: EventInputModel): EventModel => {
+    const newEventModel: EventModel = {
+      eventId: input.eventId.S,
+      event_name: input.event_name.S,
+      description: input.description.S,
+      date: input.date.S,
+      host_name: input.host_name.S ? input.host_name.S : undefined,
+      location: {
+        street: input.location ? input.location.M.street.S : undefined,
+        city: input.location ? input.location.M.city.S : undefined,
+        state: input.location ? input.location.M.state.S : undefined,
+        zipCode: input.location ? input.location.M.zipCode.S : undefined,
+        country:
+          input.location && input.location.M.country
+            ? input.location.M.country.S
+            : undefined,
+      },
+      website: input.website.S ? input.website.S : undefined,
+      registration_link: input.registration_link.S
+        ? input.registration_link.S
+        : undefined,
+      phone_number: input.phone_number.S ? input.phone_number.S : undefined,
+      picture: input.picture
+        ? input.picture.L.map((url: { S: string }) => url.S)
+        : undefined,
     };
+
+    return newEventModel;
+  };
 }
