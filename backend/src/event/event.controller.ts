82015fbd345148e52ee973a675069df28ca0bb5f
neetidesai <97416423+neetidesai@users.noreply.github.com>
2025-03-25 11:45:56 -0400

    Get and post events

--- backend/src/event/event.controller.ts
+++ backend/src/event/event.controller.ts
@@ -1,7 +1,31 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
 import { EventService } from './event.service';
+import { NewEventInput } from '../dtos/newEventDTO';
 
 @Controller('events')
 export class EventController {
   constructor(private readonly eventService: EventService) {}
+
+  @Post()
+  public async postShelter(@Body() eventData: NewEventInput) {
+    return await this.eventService.postEvent(eventData);
+  }
+
+  @Get()
+  public async getEvents() {
+    return await this.eventService.getEvents();
+  }
+
+  @Get('/:eventId')
+  public async getEvent(@Param('eventId') eventId: string) {
+    return await this.eventService.getEvent(eventId);
+  }
 }
