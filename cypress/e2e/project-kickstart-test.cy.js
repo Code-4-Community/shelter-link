27174d06f69b8754c2bcaf7f24b871788b688197
Sam Nie <147653722+SamNie2027@users.noreply.github.com>
2025-06-11 20:28:17 -0400

    Update project-kickstart-test.cy.js

--- cypress/e2e/project-kickstart-test.cy.js
+++ cypress/e2e/project-kickstart-test.cy.js
@@ -2,18 +2,44 @@
 /// <reference types="cypress" />
 
 
-describe('My First Test', () => {
+describe('Validating content of the landing page', () => {
+
+    Cypress.on('uncaught:exception', (err, runnable) => {
+        // returning false here prevents Cypress from
+        // failing the test
+        return false
+    })
+
+
+    before(() => {
+        //iPhone 16 Pixel heights divided by 2 since the Cypress presets are generally divided by 2
+        cy.viewport(590, 1278)
+
+    });
 
     beforeEach(() => {
+        cy.visit('http://localhost:8081/');
+    });
 
-        cy.visit('http://localhost:3000/');
-      })
 
+    it('should display the ShelterLink heading', () => {
+        cy.contains('ShelterLink')
+    });
 
-    it('Verify Text', () => {
+    it('should display the description text', () => {
+        cy.contains('Connecting LGBTQ+ individuals with safe shelters throughout Boston');
+    });
 
-        cy.get("div[id='root']").should('have.text', 'React Native testing with Cypress');
+    it('should display the Create Account text', () => {
+        cy.contains('Create Account');
+    });
 
+    it('should display the Log In text', () => {
+        cy.contains('Log In');
+    });
+
+    it('should display the Continue as Guest text', () => {
+        cy.contains('Continue as Guest');
     });
 
 })
